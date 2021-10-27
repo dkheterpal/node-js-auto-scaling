fc2bae6b3f60d730fcd632c0e0ef52ad69895ae3
dkheterpal <khetarpal.mca@gmail.com>
2021-10-27 19:57:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Auto Scaling Demo App</h1> <h4>Message: Success</h4> <p>Version: 1.0.0</p>');
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const absoURL = req.protocol + '://' + req.get('host') + req.originalUrl;
+  res.send(`<h1>Auto Scaling Demo App</h1> <h4>Message: Success</h4> <p>Version: 1.0.0</p><p>IP: ${ip} <p>URL: ${req.url}<p>Absolute URL: ${absoURL}`);
 })
 
 app.listen(port, () => {
-  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-console.log(`Demo app is up and listening to port ${port} and the ip is ${ip}`);
+  console.log(`Demo app is up and listening to port ${port}`);
 })
