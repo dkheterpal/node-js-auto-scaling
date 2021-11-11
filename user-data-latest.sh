dd1a1be5365acc44da5ea64c6bf9a545fa2930bd
dkheterpal <khetarpal.mca@gmail.com>
2021-11-11 19:34:10 +0530

    Create user-data-latest.sh

---
+++ user-data-latest.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -ex
+# output user data logs into a separate file for debugging
+exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+# download nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+# source nvm
+. /.nvm/nvm.sh
+# install node
+nvm install node
+#export NVM dir
+export NVM_DIR="/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
+#upgrade yum
+sudo yum update -y
+#install git
+sudo yum install git -y
+cd /home/ec2-user
+# get source code from github
+git clone https://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:x-oauth-basic@github.com/study-r/studyr-backend.git
+git checkout dev
+#get in project dir
+cd studyr-backend
+#give permission
+sudo chmod -R 755 .
+#install node module
+npm install
+# start the app
+npm start > app.out.log 2> app.err.log < /dev/null &
